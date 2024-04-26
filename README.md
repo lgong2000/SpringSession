6f0146e3f5e1409035e06a5aaefa4785ec1af4da
lgong2000 <lgong2000@gmail.com>
2024-04-26 15:05:49 -0500

    Docker Compose and sessetion stored in redis

--- README.md
+++ README.md
@@ -13,3 +13,31 @@ Username: user
 Chrome - http://localhost:8080/
 Inspect - Application - Storage - Cookies - JSESSIONID
 Every time the Application Server Restart - Session Changed
+
+Version 2
+#error spring Cannot connect to the Docker daemon at npipe:////./pipe/dock
+  -> Docker Service(Docker Desktop) is needed
+
+Docker Compose need Spring Boot 3.1+
+pom.xml
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-docker-compose</artifactId>
+    </dependency>
+
+docker-compose.yml
+    version: '3.2'
+    services:
+    redis:
+    image: "redis:alpine"
+    ports:
+    - "6379:6379"
+
+application.properties
+    spring.docker.compose.lifecycle-management=start_only
+
+Docker Command:
+Terminal Window:
+docker ps ##list docker image run status
+docker exec -it springsession-redis-1 redis-cli ##run a client
+    127.0.0.1:6379> keys *  ##list key
--- src/main/java/com/example/springsession/controller/HomeController.java
+++ src/main/java/com/example/springsession/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.springsession.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,19 @@ import java.security.Principal;
 
 @RestController
 public class HomeController {
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
     @GetMapping("/")
-    public String home(Principal principal) {
+    public String home(Principal principal, HttpSession session) {
+        incrementCount(session, HOME_VIEW_COUNT);
         return "Hello, " + principal.getName();
+    }
+
+    @GetMapping("/count")
+    public String count(HttpSession session) {
+        return "HOME_VIEW_COUNT: " + session.getAttribute(HOME_VIEW_COUNT);
+    }
+    private void incrementCount(HttpSession session, String attr) {
+        var homeViewCount = session.getAttribute(attr) == null ? 0 : (Integer)session.getAttribute(attr);
+        session.setAttribute(attr, homeViewCount += 1);
     }
 }
